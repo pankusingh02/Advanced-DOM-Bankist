d0ad2d59ca3c797c7c25b35375e3b0f7039559b1
pankusingh02 <pankusingh02@gmail.com>
2023-09-28 20:46:17 +0530

    first commit

--- script.js
+++ script.js
@@ -1,82 +1,31 @@
-// Coding Challenge #1
+'use strict';
 
-/* 
-We're building a football betting app (soccer for my American friends 😅)!
+///////////////////////////////////////
+// Modal window
 
-Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-1. Create one player array for each team (variables 'players1' and 'players2')
-2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, 
-and one array ('fieldPlayers') with all the remaining 10 field players
-3. Create an array 'allPlayers' containing all players of both teams (22 players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 
-'Coutinho' and 'Perisic'
-5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of
- goals that were scored in total (number of player names passed in)
-7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
-
-TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
-
-GOOD LUCK 😀
-*/
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer","Pavard","Martinez","Alaba","Davies","Kimmich","Goretzka","Coman","Muller","Gnarby","Lewandowski",],
-    [
-      "Burki","Schulz","Hummels","Akanji","Hakimi","Weigl","Witsel","Hazard","Brandt","Sancho","Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
-//#Task1
-const players1=[...game.players[0]]
-console.log("players1================>",players1)
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-const players2=[...game.players[1]];
-console.log("players2================>",players2)
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-//Task2
-const [gk,...fieldPlayers]=players1;
-console.log('gk=================>',gk,fieldPlayers)
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-const [gk2,...fieldPlayers2]=players2;
-console.log('gk=================>',gk2,fieldPlayers)
-
-//Task3
-const allPlayers=[...players1,...players2]
-console.log("allPlayers============>",allPlayers)
-
-//Task4
-const players1Final=['Thiago', 'Coutinho', 'Perisic',...players1];
-console.log("players1Final============>",players1Final)
-
-//Task5
-const { odds: { team1, x: draw, team2 }}=game
-console.log(team1,draw,team2)
-
-//Task6
- function printGoals(...playersgetting){
-console.log('playersgetting==============>',playersgetting)
-console.log(`${playersgetting.length} goals were scored`);
-
-}
-
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-
-printGoals(...game.scored);
-
-// Task7.
-team1 < team2 && console.log('Team 1 is more likely to win');
-team1 > team2 && console.log('Team 2 is more likely to win');
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
