45e0dcf28dd0eca7d62ae5c4e9efd212d355b223
pankusingh02 <pankajaswal2020@gmail.com>
2023-10-22 01:38:59 +0530

    stickt header and menu fade

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const nav = document.querySelector('.nav');
+
 
 const openModal = function () {
   modal.classList.remove("hidden");
@@ -137,4 +139,76 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {e.
       behavior: "smooth"
     });
   }
+});
+
+//Tabbed Component
+
+const tabs=document.querySelectorAll('.operations__tab');
+const tabsContainer =document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click',function (e){
+
+  let clicked= e.target.closest('.operations__tab')
+
+  console.log("clicked =====>",clicked)
+
+//Guard clause
+if(!clicked) return;
+
+//Remove active classes
+
+tabs.forEach(t=>t.classList.remove('operations__tab--active'));
+tabsContent.forEach(c=>c.classList.remove('operations__content--active'))
+
+
+//Activate tab
+clicked.classList.add( 'operations__tab--active' )
+
+//Activate content area
+document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+.classList.add('operations__content--active')
+
+});
+
+//Menu fade animation
+
+const handleHover = function (e,opacity){
+  if(e.target.classList.contains('nav__link')){
+
+    const link  = e.target;
+
+    console.log("target hadlehover=====>", link)
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link')  ;
+
+    console.log("siblings======>", siblings)
+    const logo =link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el=>{
+      if(el !==link) el.style.opacity= opacity;
+    });
+
+    logo.style.opacity= opacity;
+  }
+}
+
+//Passing "Arguments" into handler
+
+nav.addEventListener("mouseover", function(e){
+  handleHover(e,0.5)
+});
+nav.addEventListener("mouseout", function(e){
+  handleHover(e,1)
+});
+
+/**Implementing a sticky navigation */
+
+const intialCords = section1.getBoundingClientRect();
+console.log(intialCords);
+
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+
+  if (window.scrollY > intialCords.top) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
 });
